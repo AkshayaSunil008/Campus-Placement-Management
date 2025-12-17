e3307ddc5d57099077bc82d8e8c6711d13c54264
AkshayaSunil008 <akshayasunil008@gmail.com>
2025-12-17 19:22:03 +0530

    Delete src/main/panelistdrive/PanelistDriveRepository.java

--- src/main/panelistdrive/PanelistDriveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.model.panelistdrive;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.application.model.panelistdrive.PanelistDrive;
-
-@Repository
-public interface PanelistDriveRepository extends JpaRepository<PanelistDrive, Integer> {
-}
